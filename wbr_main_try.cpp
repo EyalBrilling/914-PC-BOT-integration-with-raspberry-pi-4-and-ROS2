3fc378e1d4d28877d9abfec746ed175bb1297e0b
Eyal Brilling <EyalBrilling@gmail.com>
2023-08-27 14:04:20 +0300

    - Using "updateM3" as needed.

--- wbr_main_try.cpp
+++ wbr_main_try.cpp
@@ -3,11 +3,17 @@ int main(){
     wbr914_minimal wbr914;
 
     wbr914.MainSetup();
+    wbr914.UpdateM3();
     bool test = wbr914.EnableMotors(true);  
+    wbr914.UpdateM3();
     if (test){
-        printf("good");
+        printf("good\n");
     }
-    // sleep(5);
+    //wbr914.SetVelocity(10,10);
+    wbr914.SetContourMode( VelocityContouringProfile );
+    wbr914.SetVelocityInTicks(10,10);
+    sleep(1);
+    wbr914.SetVelocity(0,0);
     wbr914.MainQuit();
     //wbr914.SetVelocity(1,1);
 
