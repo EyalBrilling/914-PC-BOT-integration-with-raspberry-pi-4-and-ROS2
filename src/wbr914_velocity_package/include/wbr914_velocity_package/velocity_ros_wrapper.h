2797b192418ec11588dafa6d2f96c32e99dfa1a0
Eyal Brilling <EyalBrilling@gmail.com>
2023-08-31 19:36:32 +0300

    - subscriber compiles with Twist. - put de/constructor in header - velocity_callback imple in cpp

--- src/wbr914_velocity_package/include/wbr914_velocity_package/velocity_ros_wrapper.h
+++ src/wbr914_velocity_package/include/wbr914_velocity_package/velocity_ros_wrapper.h
@@ -1,25 +1,48 @@
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int16.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "wbr914_minimal.h"
 
+using std::placeholders::_1;
+
+/*
+  A ROS listener(subscriber) reading Twist messages from the 'velocity' topic.
+  Only the x value of the linear and angular velocity is used.
+*/
 class CmdVelListener : public rclcpp::Node{
 public:
-  CmdVelListener();
-  ~CmdVelListener();
+  CmdVelListener() : Node("vel_listener_node"){
+    // Open connection with wbr914 usb and enable motors
+    wbr914.MainSetup();
+    wbr914.UpdateM3();
+    wbr914.EnableMotors(true);  
+    wbr914.UpdateM3();
+
+    // Create the subscriber to a 'velocity' topic.
+    // Using placeholder _1 for keeping the option of using a ptr to Twist.
+    sub = this->create_subscription<geometry_msgs::msg::Twist>(
+      "velocity",10,std::bind(&CmdVelListener::velocity_callback,this,_1));
+  };
+
+  ~CmdVelListener(){
+    // Shutdown communication with wbr914
+    wbr914.MainQuit();
+  };
+
+private:
 /*
  Twist message is expected in the cmd_vel topic
  linear:
-   x: 2.0
+   x: X
    y: 0.0
    z: 0.0
  angular:
-   x: 0.0
+   x: X
    y: 0.0
    z: 0.0
 */
-  void OnCmd(const geometry_msgs::Twist& twist);
+  void velocity_callback(const geometry_msgs::msg::Twist& msg);
 
-private:
-  ros::Subscriber sub;
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub;
   wbr914_minimal wbr914;
 };
