c616d711bd974d5900177833b701145da1b2b4b1
Eyal Brilling <EyalBrilling@gmail.com>
2023-09-13 19:06:16 +0300

    -  Testing both getting of velocity and position - in additions to setting velocity

--- wbr_main_try.cpp
+++ wbr_main_try.cpp
@@ -10,15 +10,19 @@ int main(){
     if (test){
         printf("good\n");
     }
-    int32_t left;
-    int32_t right;
+    int32_t leftVel;
+    int32_t rightVel;
+    int32_t leftPos;
+    int32_t rightPos;
     //wbr914.SetVelocity(10,10);
     wbr914.SetContourMode( VelocityContouringProfile );
     for(int i=0;i<=1000;i++){
-    wbr914.SetVelocityInTicks(10000,10000);
+    wbr914.SetVelocityInTicks(10000,5000);
     wbr914.UpdateM3();
-    wbr914.GetVelocityInTicks(&left,&right);
-    printf("left:%i right:%i Velocities ticks\n",left,right);
+    wbr914.GetVelocityInTicks(&leftVel,&rightVel);
+    printf("left:%i right:%i Velocities ticks\n",leftVel,rightVel);
+    wbr914.GetPositionInTicks(&leftPos,&rightPos);
+    printf("left:%i right:%i Positions in ticks\n",leftPos,rightPos);
     }
     sleep(1);
     wbr914.SetVelocity(0,0);
