7849f19b0b1ed330222a5474274f5aff9b460fc7
Eyal Brilling <EyalBrilling@gmail.com>
2023-09-25 21:50:45 +0300

    - Removed unneede line in examples

--- src/node_examples/src/velocity_publisher_continuous_basic.cpp
+++ src/node_examples/src/velocity_publisher_continuous_basic.cpp
@@ -4,8 +4,6 @@
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
 
-  CmdVelPublisher_continuous_basic my_publisher_node;
-
   // Spin until ROS is shutdown
   rclcpp::spin(std::make_shared<CmdVelPublisher_continuous_basic>());
 
