1efbd316e1307df3bff9cf64f4ca0e29c8a72811
Eyal Brilling <EyalBrilling@gmail.com>
2023-10-05 15:56:39 +0300

    - First iteration of wiggle from side to side. Getting linear and velocity from user(might remove linear) using opposite angular velocity as the wiggle movement

--- src/node_examples/include/node_examples/wiggle_robot.h
+++ src/node_examples/include/node_examples/wiggle_robot.h
@@ -12,26 +12,14 @@ const int velocity_command_secs = 10;
 class Wiggle_robot: public rclcpp::Node {
 public:
   Wiggle_robot() : Node("Wiggle_robot") {
-    // Create a publisher to the 'velocity' topic
+      // Create a publisher to the 'velocity' topic
     publisher = this->create_publisher<geometry_msgs::msg::Twist>(velocity_topic_name, 10);
-
-    geometry_msgs::msg::Twist msg;
-    msg.linear.x = 0;
-    msg.angular.x = 0;
-
-    // Open new thread to continually publish Twist msg.
-    std::thread thread([this,&msg](){
-        while(rclcpp::ok()){
-          publisher->publish(msg);
-        }
-      });
-    while (rclcpp::ok()) {
-
+    // Create a 'while' loop that wiggle the robot until node exits with ctrl+c
       // Read input from the user
       std::string input;
       std::vector< std::string > tokens;
       std::string token;
-      printf("Enter x linear velocity(m/s) and x angular velocity(rads/sec) or exit to stop publisher: ");
+      printf("Enter x linear velocity(m/sec) and x angular velocity(rads/sec) or exit to stop publisher: ");
 
       // Get input from user and save in a vector
       std::getline(std::cin, input);
@@ -41,28 +29,38 @@ public:
 
       
       }
-      if(input == "exit"){
-        rclcpp::shutdown();
-        break;
-      }
 
       // Expecting only x of linear and angular velocities
       if(tokens.size() != 2){
-          printf("input error. 2 values are expected.\n");
-          // For safety,in case of input error reset movement
-          msg.linear.x = 0;
-          msg.angular.x = 0;
-          continue;
+        printf("input error. 2 values are expected.\n");
+        exit(0);
       }
-        // Construct message.
+        // Construct message
+        geometry_msgs::msg::Twist msg;
         msg.linear.x = std::stod(tokens[0]);
         msg.angular.x = std::stod(tokens[1]);
+        // Run infinitly,wiggling from side to side.
+        while (rclcpp::ok()) {
+        // Run the command for some seconds. Robot expects contiunally command
+        auto startTime = std::chrono::steady_clock::now();
+        auto endTime = startTime + std::chrono::seconds(velocity_command_secs);
 
+        printf("Publishing for %i secs to topic %s Linear x: %f, Angular x: %f \n", velocity_command_secs, velocity_topic_name.c_str(), msg.linear.x, msg.angular.x);
 
-        printf("Publishing to topic %s Linear x: %f, Angular x: %f \n", velocity_topic_name.c_str(), msg.linear.x, msg.angular.x);
+        while (std::chrono::steady_clock::now() < endTime) {
+            publisher->publish(msg);
         }
+        startTime = std::chrono::steady_clock::now();
+        endTime = startTime + std::chrono::seconds(velocity_command_secs);
 
-        thread.join();
+        msg.angular.x = -msg.angular.x;
+        printf("Publishing other side movement\n");
+        printf("Publishing for %i secs to topic %s Linear x: %f, Angular x: %f \n", velocity_command_secs, velocity_topic_name.c_str(), msg.linear.x, msg.angular.x);
+        while (std::chrono::steady_clock::now() < endTime) {
+            publisher->publish(msg);
+        }
+            
+        }
   }
 
 private:
